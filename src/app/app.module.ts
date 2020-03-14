ab20f51bf198a9143939cc0bc5f477cf9fe577cf
Alexis <adeudelon@ITRUST-LAP-035.domitr.itrust.lu>
2020-03-14 14:33:32 +0100

    update with component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TestCComponent } from './test-c/test-c.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestCComponent
   ],
   imports: [
     BrowserModule
